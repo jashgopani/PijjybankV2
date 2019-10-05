a08ca43881fb1f12718c835bb8de5464fe23346d
Jash <jash.gopani@somaiya.edu>
2019-10-05 15:14:26 +0530

    Directory re-structured

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-//import 'timeline.dart';
+import 'screens/login.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,70 +20,3 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-class Login extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.blue,
-        body: Center(
-          child: Container(
-            margin: EdgeInsets.symmetric(horizontal: 40.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.all(40.0),
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(
-                        color: Colors.white,
-                        width: 6.0,
-                      )),
-                  child: Text(
-                    "Pb",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 90.0,
-                      fontWeight: FontWeight.w400,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                RaisedButton(
-                  elevation: 3.0,
-                  color: Colors.white,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0)),
-                  padding: EdgeInsets.symmetric(vertical: 10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      GoogleIcon(),
-                      Container(
-                          padding: EdgeInsets.symmetric(horizontal: 5.0),
-                          child: Text(
-                            "Sign in with Google",
-                            style: TextStyle(fontSize: 20.0),
-                          ))
-                    ],
-                  ),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ),
-        ));
-  }
-}
-
-class GoogleIcon extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    AssetImage ai = AssetImage("images/googleIcon.png");
-    Image image = Image(
-      height: 28.0,
-      image: ai,
-    );
-    return image;
-  }
-}
