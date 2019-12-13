05cb46de9fff1c7f043b341b518c04db7274d5f5
Jash Gopani <jash.gopani@somaiya.edu>
2019-12-13 18:19:11 +0530

    new timeline

--- lib/screens/timeline.dart
+++ lib/screens/timeline.dart
@@ -1,251 +1,140 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:pijjybank/models/transaction.dart';
-import 'package:pijjybank/screens/groups.dart';
-import 'package:side_header_list_view/side_header_list_view.dart';
+import 'package:flutter/widgets.dart';
 
 class Timeline extends StatefulWidget {
   final FirebaseUser user;
 
-  Timeline({
-    @required this.user
-  });
+  Timeline({@required this.user});
 
   @override
   _TimelineState createState() => _TimelineState();
 }
 
 class _TimelineState extends State<Timeline> {
+  var tabs = ["Recent Transactions", "Expense Groups", "Account Summary"];
+  var screens = [
+    Container(padding: EdgeInsets.all(100.0),
+        color: Colors.green,
+        child: Text("timeline")),
+    Container(padding: EdgeInsets.all(100.0),
+        color: Colors.green,
+        child: Text("groups")),
+    Container(padding: EdgeInsets.all(100.0),
+        color: Colors.green,
+        child: Text("summary")),
+  ];
+  int currentScreen = 0;
 
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-  final GlobalKey<ScaffoldState> _drawerKey =
-      GlobalKey<ScaffoldState>(); //for drawer
-
-  PersistentBottomSheetController _bottomSheetController;
-  String _username = "Jash";
-
-  var fabIcon = Icon(Icons.add);
-  var elev = 6.0;
-
-
-  _getDay(int weekday) {
-    if (weekday == 1)
-      return "MON";
-    else if (weekday == 2)
-      return "TUE";
-    else if (weekday == 3)
-      return "WED";
-    else if (weekday == 4)
-      return "THU";
-    else if (weekday == 5)
-      return "FRI";
-    else if (weekday == 6)
-      return "SAT";
-    else
-      return "SUN";
-  }
-
-  Container _buildBottomSheet(context) {
+  Widget _appbar() {
     return Container(
-      height: MediaQuery.of(context).size.height * 0.7,
-      width: MediaQuery.of(context).size.width,
-      padding: EdgeInsets.all(8.0),
-      decoration: BoxDecoration(
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black,
-            offset: Offset(0, 3.0),
-            blurRadius: 5.0
-          )
-        ],
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(16.0), topRight: Radius.circular(16.0))),
-      child: Column(
+      padding: EdgeInsets.all(16.0),
+      margin: EdgeInsets.only(top: 16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          Text("Bottom Sheet"),
-          RaisedButton(
-              child: Text("CLose"),
-              onPressed: () {
-                _closeBottomSheet();
-              })
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                "Your Balance",
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+              Text(
+                "Rs.6900",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 32.0),
+              )
+            ],
+          ),
+          CircleAvatar(
+            radius: 24.0,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(50.0),
+              child: Image.network(
+                'https://picsum.photos/250?image=9',
+              ),
+            ),
+          )
         ],
       ),
     );
   }
 
-  _closeBottomSheet() {
-    if (_bottomSheetController != null) {
-      setState(() {
-        elev=6.0;
-        fabIcon = Icon(Icons.add);
-      });
-      _bottomSheetController.close();
-      _bottomSheetController = null;
-    }
+  Widget _tabs() {
+    return Expanded(
+      flex: 1,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10.0),
+        child: Stack(
+            children: <Widget>[ ListView.builder(
+              itemCount: tabs.length,
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (BuildContext context, int item) {
+                return Padding(
+                  padding: const EdgeInsets.only(left: 16.0, right: 24.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        currentScreen = item;
+                      });
+                    },
+                    child: Text(
+                      tabs[item],
+                      style: TextStyle(
+                        color: item == currentScreen ? Colors.blue : Colors
+                            .grey,
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+            ]
+        ),
+      ),
+    );
+  }
+
+  Widget _showScreen() {
+    return Expanded(
+      flex: 15,
+      child: GestureDetector(
+        onHorizontalDragEnd: (DragEndDetails details) {
+          setState(() {
+            print("gesture");
+            currentScreen = (currentScreen + 1) % tabs.length;
+          });
+        },
+        child: screens[currentScreen],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    String name = this.widget.user.displayName;
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-          _username,
-          ),
-          elevation: 1.0,
-          iconTheme: new IconThemeData(color: Colors.white),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {},
-            ),
-          ],
-          flexibleSpace: Container(),
-        ),
-        key: _scaffoldKey,
-        body: RefreshIndicator(
-          onRefresh: () async {
-            //as this always returns a future object, we have just added a future delay and
-            // await keyword by default returns a future object so no need to return explicitly here
-            await Future.delayed(Duration(seconds: 1));
-            _scaffoldKey.currentState.showSnackBar(SnackBar(
-              content: Text("Refreshed"),
-            ));
-          },
-          child: SideHeaderListView(
-            padding: new EdgeInsets.all(8.0),
-            itemCount: transactions.length,
-            itemExtend: 70.0,
-            headerBuilder: (BuildContext context, int index) {
-              return Container(
-                  padding: EdgeInsets.all(8.0),
-                  decoration: BoxDecoration(),
-                  child: Column(
-                    children: <Widget>[
-                      Text(_getDay(transactions[index].date.weekday)),
-                      Text(
-                        transactions[index].date.day.toString(),
-                        style: Theme.of(context).textTheme.headline,
-                      ),
-                    ],
-                  ));
-            },
-            itemBuilder: (BuildContext context, int index) {
-              return Container(
-                margin: EdgeInsets.only(bottom: 8.0, left: 8.0),
-                padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(8.0),
-                      bottomRight: Radius.circular(8.0),
-                      bottomLeft: Radius.circular(8.0)),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          transactions[index].title,
-                          style: Theme.of(context).textTheme.title,
-                        ),
-                        Text(
-                          transactions[index].party,
-                          style: Theme.of(context).textTheme.subtitle,
-                        )
-                      ],
-                    ),
-                    Text(
-                      transactions[index].amount.toString(),
-                      style: Theme.of(context).textTheme.headline,
-                    )
-                  ],
-                ),
-              );
-            },
-            hasSameHeader: (int a, int b) {
-              Duration val =
-                  transactions[a].date.difference(transactions[b].date);
-              if (val.inDays == 0) return true;
-              return false;
-            },
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          elevation: elev,
-          onPressed: () {
-            if (_bottomSheetController == null) {
-              setState(() {
-                elev=0.0;
-                fabIcon = Icon(Icons.close);
-              });
-              _bottomSheetController = _scaffoldKey.currentState
-                  .showBottomSheet((context) => _buildBottomSheet(context));
-            } else {
-              _closeBottomSheet();
-            }
-
-          },
-          child: fabIcon,
-          tooltip: "Add new transaction",
-        ),
-        drawer: Drawer(
-            key: _drawerKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                DrawerHeader(
-                  padding: EdgeInsets.zero,
-                  duration: const Duration(milliseconds: 30000),
-                  child: UserAccountsDrawerHeader(
-                    accountEmail: Text("jash@gmail.com",
-                        style: TextStyle(
-                          fontSize: 15.0,
-                        )),
-                    accountName: Text(_username,
-                        style: TextStyle(
-                            fontSize: 25.0, fontWeight: FontWeight.w600)),
-                  ),
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                  ),
-                ),
-                ListTile(
-                  leading: Icon(Icons.people),
-                  title: Text("Group"),
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return Groups();
-                    }));
-                  },
-                ),
-                ListTile(
-                  leading: Icon(Icons.timeline),
-                  title: Text("Timeline"),
-                ),
-                ListTile(
-                  leading: Icon(Icons.insert_chart),
-                  title: Text("Statistics"),
-                ),
-                ListTile(
-                    leading: Icon(Icons.account_circle),
-                    title: Text("Profile")),
-                ListTile(
-                  leading: Icon(Icons.exit_to_app),
-                  title: Text("Logout"),
-                ),
-              ],
-            )),
-//        floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.blue,
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+      body: Column(
+        children: <Widget>[
+          _appbar(),
+          _tabs(),
+          _showScreen()
+        ],
       ),
     );
   }
